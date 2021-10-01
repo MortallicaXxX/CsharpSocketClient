050798225a39c45e90e074929038c634e83f5f9f
MortallicaXxX <MortallicaXxX@hotmail.be>
2021-10-01 14:57:50 +0200

    Ajout gestion des chanels

--- CsharpSocketClient/CsharpSocketClient/Program.cs
+++ CsharpSocketClient/CsharpSocketClient/Program.cs
@@ -4,27 +4,94 @@ using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Text;
 using System.Collections.Generic;
+using System.Text.Json;
 
 namespace socketCsharp
 {
 
+    public class Message
+    {
+
+        private string? _chanel = null;
+        private string? _message = null;
+
+        private struct M
+        {
+            public string chanel { get; set; }
+            public string data { get; set; }
+        }
+
+        public string message { get { return _message; } }
+        public string chanel { get { return _chanel; } }
+
+        public Message(string message)
+        {
+            this._normalize(message);
+        }
+
+        private void _normalize(string message)
+        {
+            try
+            {
+                M m = JsonSerializer.Deserialize<M>(message) ;
+                this._chanel = m.chanel;
+                this._message = m.data;
+            }
+            catch(Exception e)
+            {
+                this._message = message;
+            }
+        }
+    }
+
     class socketChanels
     {
 
-        private List<Func<dynamic>> callBacks = new List<Func<dynamic>>();
+        private List<Func<dynamic , Message , bool>> callBacks = new List<Func<dynamic , Message , bool>>();
         private List<string> chanelsNames = new List<string>();
+        private dynamic root;
 
-        public socketChanels()
+        public socketChanels(dynamic root)
         {
-
+            this.root = root ?? throw new ArgumentNullException(nameof(root));
         }
 
-        public void Add(string chanelName, Func<dynamic> callback)
+        public List<string> Chanels { get { return chanelsNames; } }
+
+        public void Exec(Message message)
+        {
+            try
+            {
+                if ((message.chanel != null) && (this.chanelsNames.Contains(message.chanel))) _exec(message);
+                else throw new Exception("Ce chanel n'est pas définis");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void Add(string chanelName, Func<dynamic , Message , bool> callback)
         {
             this._add(chanelName,callback);
         }
 
-        private void _add(string chanelName , Func<dynamic> callback)
+        public bool Delete(string chanelName)
+        {
+            int x = this._getIdFromName(chanelName);
+            if(x > -1)
+            {
+                this.callBacks.RemoveAt(x);
+                this.chanelsNames.RemoveAt(x);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void _add(string chanelName , Func<dynamic , Message , bool> callback)
         {
             if(this._isChanelExist(chanelName) == false)
             {
@@ -33,16 +100,21 @@ namespace socketCsharp
             }
         }
 
-        private bool _isChanelExist(string name)
+        private bool _isChanelExist(string chanelName)
+        {
+            return this.chanelsNames.Contains(chanelName);
+        }
+
+        private int _getIdFromName(string name)
         {
             for (int i = 0; i < this.chanelsNames.Count; i++)
             {
-                if (this.chanelsNames[i] == name) return true;
+                if (this.chanelsNames[i] == name) return i;
             }
-            return false;
+            return -1;
         }
 
-        private Func<dynamic> _getCallBackFromName(string name)
+        private Func<dynamic , Message , bool> _getCallBackFromName(string name)
         {
             for(int i = 0; i < this.chanelsNames.Count; i++)
             {
@@ -51,12 +123,17 @@ namespace socketCsharp
             return null;
         }
 
+        private void _exec(Message message)
+        {
+            this.callBacks[this._getIdFromName(message.chanel)](this.root , message);
+        }
+
     }
 
     // class corespondant au module capable de capter les message et de répondre
     class socketCaptor
     {
-        private int port;
+        private int port;                   //
         private IPHostEntry ipHost;         //
         private IPAddress ipAddr;           //
         private IPEndPoint localEndPoint;   //
@@ -67,7 +144,7 @@ namespace socketCsharp
 
         private int nbrUsers;               // nombre d'utilisateur max présent dans la liste Socket.Listen()
 
-        private socketChanels chanels = new socketChanels();
+        private socketChanels chanels;
 
         /* @{name}      socketEmitor
          * @{type}      public constructor
@@ -80,10 +157,13 @@ namespace socketCsharp
         {
             this.nbrUsers = (nbrUsers != null ? (int)nbrUsers : 10);
             this.port = (port != null ? (int)port : 11111);
+            this.chanels = new socketChanels(this);
             this._setEndpoint();
             this._setListener();
             this.serverTask = new Task(this._execServer);
         }
+
+        public socketChanels Chanels(){ return this.chanels; }
 
         /*
          * @{name}      Start
@@ -95,19 +175,38 @@ namespace socketCsharp
             this.serverTask.Start();
         }
 
+        /*
+          * @{name}      IP
+          * @{type}      public IPAddress
+          */
         public IPAddress IP()
         {
             return this.ipAddr;
         }
 
+        /*
+          * @{name}      Port
+          * @{type}      private void
+          */
         public int Port()
         {
             return this.port;
         }
 
-        public void On(string chanel, Func<dynamic> callback)
+        /*
+         * @{name}      On
+         * @{type}      public int
+         */
+        public void On(string chanel, Func<dynamic , Message , bool> callback)
         {
             this.chanels.Add(chanel, callback);
+        }
+
+        public bool Reply(string message)
+        {
+            this._send(message);
+            this._close();
+            return true;
         }
 
         /*
@@ -147,7 +246,7 @@ namespace socketCsharp
          * @{type}      private void
          * @{desc}      WriteLine du résultat de la donnée reçue
          */
-        private string _Onmessage()
+        private Message _Onmessage()
         {
             // Data buffer
             byte[] bytes = new Byte[1024];
@@ -158,14 +257,13 @@ namespace socketCsharp
 
                 int numByte = this.clientSocket.Receive(bytes);
 
-                data += Encoding.ASCII.GetString(bytes,
-                                           0, numByte);
+                data += Encoding.ASCII.GetString(bytes,0, numByte);
 
                 if (data.IndexOf("<EOF>") > -1)
                     break;
             }
 
-            return data;
+            return new Message(data.Split("<EOF>")[0]);
         }
 
         /*
@@ -173,11 +271,17 @@ namespace socketCsharp
          * @{type}      private void
          * @{desc}      Affichage en bleu d'un message dans la console.
          */
-        private void _print(string message)
+        private void _print(Message data)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Message recive : {0} ", message);
+            Console.WriteLine((data.chanel == null ? $"General chanel receive : {data.message}" : $"{data.chanel} chanel recive : {data.message}"));
             Console.ResetColor();
+            if (data.chanel != null) this.chanels.Exec(data);
+            else
+            {
+                this._send("true");
+                this._close();
+            }
         }
 
         /*
@@ -195,23 +299,24 @@ namespace socketCsharp
                 // En utilisant la méthode Listen(), nous créons la liste des clients qui voudront se connecter au serveur.
                 this.listener.Listen(this.nbrUsers);
 
-                while (true)
-                {
-
-                    // Attente de la connexion entrante. À l’aide de la méthode Accept(), le serveur acceptera la connexion du client.
-                    this.clientSocket = this.listener.Accept();
-
-                    // Affichage du message entrant
-                    this._print(this._Onmessage());
-
-                    this._send("Yeaaa ca marche");
-                    this._close();
-                }
+                this._accept();
 
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void _accept()
+        {
+            while (true)
+            {
+                // Attente de la connexion entrante. À l’aide de la méthode Accept(), le serveur acceptera la connexion du client.
+                this.clientSocket = this.listener.Accept();
+
+                // Affichage du message entrant
+                this._print(this._Onmessage());
             }
         }
 
@@ -233,7 +338,7 @@ namespace socketCsharp
     // class corespondant au module capable de d'envoyer un message et de traiter la réponse
     class socketEmitor
     {
-
+        private string ipv6;
         private IPHostEntry ipHost;
         private IPAddress ipAddr;
         private IPEndPoint localEndPoint;
@@ -241,11 +346,24 @@ namespace socketCsharp
 
         private bool isConnected = false;
 
-        public socketEmitor(){}
+        private socketChanels chanels;
+
+        public socketEmitor(string? ipv6 = null) {
+            this.ipv6 = ipv6;
+            this.chanels = new socketChanels(this);
+        }
 
         public void Connect()
         {
             this._connect();
+        }
+
+        public void SendOn(string chanelName, string message)
+        {
+            this.Send($"{{" +
+                $"\"chanel\" : \"{chanelName}\"," +
+                $"\"data\" : \"{message}\"" +
+                $"}}");
         }
 
         public void Send(string message)
@@ -302,10 +420,12 @@ namespace socketCsharp
          */
         private void _setEndpoint()
         {
-            this.ipHost = Dns.GetHostEntry(Dns.GetHostName());
-            this.ipAddr = ipHost.AddressList[0];
+            Console.WriteLine(IPAddress.Parse(this.ipv6));
+            this.ipHost = (this.ipv6 == null ? Dns.GetHostEntry(Dns.GetHostName()) : Dns.GetHostEntry(IPAddress.Parse(this.ipv6)));
+            this.ipAddr = (ipHost.AddressList.Length > 0 ? ipHost.AddressList[0] : IPAddress.Parse(this.ipv6));
             this.localEndPoint = new IPEndPoint(ipAddr, 11111);
         }
+
 
         /*
          * @{name}      _setListener
@@ -426,9 +546,9 @@ namespace socketCsharp
     class Program
     {
 
-        private static string Test()
+        private static bool Test(dynamic cli , Message arg)
         {
-            return "test";
+            return cli.Reply("C'est ma reponse");
         }
 
         // Main Method
@@ -439,13 +559,20 @@ namespace socketCsharp
             Console.WriteLine(se.IP());     // affiche l'ip sur lequel tourne le serveur
             Console.WriteLine(se.Port());   // affiche le port sur lequel tourne le serveur
 
-            se.On("Test", new Func<dynamic>(Test));
+            se.On("Test", Test);            // création d'un chanel lié à un callback
+            se.Chanels().Delete("Test");    // suppersion du chanel
 
             se.Start();                     // Démare le serveur Socket
 
-            socketEmitor sc = new socketEmitor();
+            socketEmitor sc = new socketEmitor("fe80::45f4:89d7:78f4:68fb");
             sc.Send("Test Data");           // Envois d'un premier message
             sc.Send("Test Data2");          // Envois d'un deuxième message
+
+            // Erreur sur l'appel d'un chanel non existant - serveur se bloque A REGLER
+            //sc.SendOn("Test", "helloworld");// Envois d'un troisième message sur un chanel - Erreur puisque le chanel à été supprimer
+
+            se.On("Test", Test);            // création d'un chanel lié à un callback
+            sc.SendOn("Test", "helloworld");// Envois d'un troisième message sur un chanel - Succes puisque le chanel à été recréer
 
             Console.ReadKey();
 
